0c5dd3e7d8fdef7c4f948a9d1b9d8a1a50de2e91
2302436 <2302436@uad.ac.uk>
2025-09-30 09:43:11 +0100

    Second Pull Commit to Book Branch

--- Book.cs
+++ Book.cs
@@ -4,3 +4,13 @@ public class Book
     public List<string> Authros { get; set; }
     public DateTime PublicationDate { get; set; }
 }
+
+// Adding Random Calculator Commit to test Visual Studio Multiple branch commit rather than expand Book
+public int Add(int left, int right)
+{
+    return left + right;
+}
+public int Subtract(int left, int right)
+{
+    return left - right;
+}
