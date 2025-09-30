7f4c3d634b72c7d3079e13566b6ad9be2ef55d31
2302436 <2302436@uad.ac.uk>
2025-09-30 09:31:16 +0100

    Adding Program with correct name for week 3 practical :3

---
+++ Program.cs
@@ -0,0 +1,9 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("I just added this in Visual Studio");
+        Console.WriteLine("I just added this in the command line repo!");
+    }
+}
