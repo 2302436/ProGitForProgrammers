2a4cd972b136a9fa10a7d9972cbb421ac22c976a
2302436 <2302436@uad.ac.uk>
2025-09-30 09:43:41 +0100

    Third Commit to the Book Branch

--- Book.cs
+++ Book.cs
@@ -14,3 +14,11 @@ public int Subtract(int left, int right)
 {
     return left - right;
 }
+public int Multiply(int left, int right)
+{
+    return left * right;
+}
+public int Divide(int left, int right)
+{
+    return left / right;
+}
